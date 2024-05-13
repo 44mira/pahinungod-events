918c2404c826335072b3f9dd8d9a3d5eca860025
44mira <legolaslada@gmail.com>
2024-05-14 02:16:18 +0800

    feat: make a hook for event info page

---
+++ hooks/use-single-event-query.ts
@@ -0,0 +1,47 @@
+import { UUID } from "crypto";
+import useSupabase from "./useSupabase";
+import { useQueries } from "@tanstack/react-query";
+
+export default function useSingleEventQuery(event_id: UUID) {
+  const supabase = useSupabase();
+
+  const eventInfoQuery = async () => {
+    const { data, error } = await supabase
+      .from("events")
+      .select()
+      .eq("event_id", event_id);
+
+    if (error) {
+      console.log("An error has occurred in fetching event information.");
+      throw error;
+    }
+
+    return data;
+  };
+
+  const volunteerListQuery = async () => {
+    const { data, error } = await supabase
+      .from("event_volunteer")
+      .select()
+      .eq("event_id", event_id);
+
+    if (error) {
+      console.log(
+        "An error has occurred in fetching volunteer list for this event.",
+      );
+      throw error;
+    }
+
+    return data;
+  };
+
+  return useQueries({
+    queries: [
+      { queryKey: ["event_information", event_id], queryFn: eventInfoQuery },
+      {
+        queryKey: ["volunteer_list", event_id],
+        queryFn: volunteerListQuery,
+      },
+    ],
+  });
+}
