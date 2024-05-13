54f6c9dcf5aa113642f23a03b9ce362b134e8086
44mira <legolaslada@gmail.com>
2024-05-14 02:39:13 +0800

    refactor: create layout for events

---
+++ app/dashboard/events/addEvent.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import add_icon from "@/public/add_icon.svg";
+import AddEventForm from "./AddEventForm";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+export default function AddEvent() {
+  const pathname = usePathname();
+
+  if (pathname === "/dashboard/events")
+    return (
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button
+            className="text-xsm text-primary-foreground lg:ml-5"
+            size="sm"
+          >
+            <Image src={add_icon} alt="add icon" />
+            Add Event
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="max-w-[40%]">
+          <DialogHeader className="text-lg font-bold">Add Event</DialogHeader>
+
+          <AddEventForm />
+        </DialogContent>
+      </Dialog>
+    );
+  return <></>;
+}
---
+++ app/dashboard/events/layout.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import AddEvent from "./addEvent";
+
+export default function Events({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex flex-col gap-5 w-full">
+      <div className="lg:flex w-full items-center gap-5 hidden">
+        <h1 className="text-2xl text-primary font-bold basis-5">Events</h1>
+        <div className="flex w-full items-center gap-5">
+          <AddEvent />
+          <div className="grow" />
+          <Button className="text-xsm text-primary-foreground">Log out</Button>
+        </div>
+      </div>
+      <div className="flex flex-col items-center gap-2 lg:hidden">
+        <div className="flex gap-2 items-center self-end">
+          <Button className="text-xsm text-primary-foreground">Log out</Button>
+        </div>
+        <h1 className="text-2xl text-primary font-bold mx-auto">Events</h1>
+        <AddEvent />
+      </div>
+      {children}
+    </div>
+  );
+}
