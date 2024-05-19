7b547b2633b14745a73354fbd9be78d30240f16c
Owen Cariño <owenyoungcarino@gmail.com>
2024-05-19 11:27:54 +0800

    feat: Fetched data from supabase, and rendered in the table.

--- app/dashboard/volunteers/page.tsx
+++ app/dashboard/volunteers/page.tsx
@@ -1,3 +1,28 @@
-export default function Test() {
-  return <>Hello</>;
+import { createClient } from "@/utils/supabase/server";
+import { Volunteer, columns } from "./columns";
+import { DataTable } from "./data-table";
+
+async function getData(): Promise<Volunteer[]> {
+  const supabase = createClient();
+  let { data: volunteer, error } = await supabase.from("volunteer").select("*");
+
+  if (error) {
+    console.error(error);
+    return [];
+  }
+
+  return volunteer || [];
+}
+
+export default async function DemoPage() {
+  const data = await getData();
+
+  return (
+    <>
+      {/* Search Bar */}
+      <div className="container mx-auto py-10">
+        <DataTable columns={columns} data={data} />
+      </div>
+    </>
+  );
 }
