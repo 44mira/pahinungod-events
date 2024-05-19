61006d9213f1e3340de9c2b38466c09ba8578f93
Owen Cariño <owenyoungcarino@gmail.com>
2024-05-20 07:04:47 +0800

    refactor: add column type 'volunteer_id'.

--- app/dashboard/volunteers/columns.tsx
+++ app/dashboard/volunteers/columns.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
-
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 export type Volunteer = {
+  volunteer_id: string;
   name: string;
   occupation:
     | "Student"
@@ -19,6 +19,10 @@ export type Volunteer = {
 };
 
 export const columns: ColumnDef<Volunteer>[] = [
+  {
+    accessorKey: "volunteer_id",
+    header: "ID",
+  },
   {
     accessorKey: "name",
     header: "Name",
