419759e909ffe2b2c2badc209b9a1b01e32a0ce4
Owen Cariño <owenyoungcarino@gmail.com>
2024-05-25 20:46:36 +0800

    feat: added UI for profile page.

---
+++ app/dashboard/profile/layout.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+
+export default function Profile({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex flex-col gap-5 w-full">
+      <div className="lg:flex w-full items-center gap-5 hidden">
+        <h1 className="text-2xl text-primary font-bold basis-5">Profile</h1>
+        <div className="flex w-full items-center gap-5">
+          <div className="grow" />
+          <Button className="text-xsm text-primary-foreground">Log out</Button>
+        </div>
+      </div>
+      <div className="flex flex-col items-center gap-2 lg:hidden">
+        <div className="flex gap-2 items-center self-end">
+          <Button className="text-xsm text-primary-foreground">Log out</Button>
+        </div>
+        <h1 className="text-2xl text-primary font-bold mx-auto">Profile</h1>
+      </div>
+      {children}
+    </div>
+  );
+}
--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,95 +1,202 @@
 "use client";
 
-import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+// Icon Imports
+import pencil_icon from "@/public/pencil_icon.svg";
+import eye_close from "@/public/eye_close.svg";
+import eye_open from "@/public/eye_open.svg";
 
-export default function Profile() {
-  const [editMode, setEditMode] = useState(true);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
+// Define form schema with password match validation
+const formSchema = z
+  .object({
+    email: z.string().min(2, {
+      message: "Email must be at least 2 characters.",
+    }),
+    password: z.string().min(6, {
+      message: "Password must be at least 6 characters.",
+    }),
+    retypePassword: z.string().min(6, {
+      message: "Password must be at least 6 characters.",
+    }),
+    name: z.string().min(6, {
+      message: "Name must be at least 6 characters.",
+    }),
+  })
+  /*  Custom logic 
+      if retypePassword does not match password.*/
+  .refine((val) => val.password === val.retypePassword, {
+    message: "Passwords do not match.",
+    path: ["retypePassword"], // Set the path of the error to the retypePassword field
+  });
 
-  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(event.target.value);
-  };
+// Define submit handler
+function onSubmit(values: z.infer<typeof formSchema>) {
+  // Do something with the form values.
+  // ✅ This will be type-safe and validated.
+  console.log(values);
+}
 
-  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
-  };
+// Edit State logic
+export default function ProfileForm() {
+  // Toggle between profile view & edit view.
+  const [editProfileState, setEditProfileState] = useState(false);
 
-  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setName(event.target.value);
-  };
+  // Show and Hide password state.
+  const [pwVisibility, setPwVisibility] = useState(false);
+
+  /* Form Types */
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      retypePassword: "",
+      name: "",
+    },
+  });
+
+  const profile = [
+    {
+      name: "Owen Young Cariño",
+      password: "random90",
+      email: "admin1234",
+    },
+  ];
 
   return (
     <>
-      <div className=" text-end text-white">
+      <span className="text-end text-white">
         <Button
-          className="text-xsm"
-          size={"sm"}
-          onClick={() => setEditMode(!editMode)}
+          size={"xs"}
+          className=""
+          onClick={() => setEditProfileState(!editProfileState)}
         >
-          {editMode ? "Save" : "Edit Profile"}
+          <Image src={pencil_icon} alt="pencil_icon" className="me-2" />
+          Edit Profile
         </Button>
-      </div>
-      <div className="w-96 space-y-9">
-        <div>
-          <Label htmlFor="username" className="font-bold text-slate-600">
-            Username
-            {editMode ? (
-              <Input
-                type="text"
-                value={username}
-                onChange={handleUsernameChange}
-                placeholder="Change username"
-                className=" bg-slate-200 border-blue-500 border-2 ms-3 font-normal placeholder:text-gray-400 "
-              />
-            ) : (
-              <p className="font-normal p-3 bg-slate-200 ms-3 rounded-sm">
-                {username}
-              </p>
-            )}
-          </Label>
+      </span>
+
+      {/* Profile View */}
+      {!editProfileState && (
+        <div className="grid grid-cols-2">
+          <div className="bg-neutral-300 rounded-md py-3 px-5 space-y-8">
+            <div className="flex justify-between">
+              <p className="font-semibold">Name</p>
+              <p className="text-end">Owen Young Cariño</p>
+            </div>
+            <div className="flex justify-between">
+              <p className="font-semibold">Email</p>
+              <p className="text-end">oycarino@up.edu.ph</p>
+            </div>
+            <div className="flex justify-between">
+              <p className="font-semibold">Password</p>
+              <div className="flex space-x-2">
+                <input
+                  type={pwVisibility ? "text" : "password"}
+                  readOnly
+                  value={"admin123"}
+                  className="border-0 outline-none bg-neutral-300 w-auto text-end"
+                />
+                <Image
+                  src={pwVisibility ? eye_open : eye_close}
+                  alt="eye open"
+                  onClick={() => setPwVisibility(!pwVisibility)}
+                  className="cursor-pointer"
+                />
+              </div>
+            </div>
+          </div>
         </div>
-        <div>
-          <Label htmlFor="password" className="font-bold text-slate-600">
-            Password
-            {editMode ? (
-              <Input
-                type="password"
-                value={password}
-                onChange={handlePasswordChange}
-                placeholder="Change password"
-                className=" bg-slate-200 border-blue-500 border-2 ms-3 font-normal placeholder:text-gray-400 "
+      )}
+
+      {/* Edit Profile */}
+      {editProfileState && (
+        <div className="grid grid-cols-3">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        placeholder="Enter your email"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
               />
-            ) : (
-              <p className="font-normal p-3 bg-slate-200 ms-3 rounded-sm">
-                {password}
-              </p>
-            )}
-          </Label>
-        </div>
-        <div>
-          <Label htmlFor="name" className="font-bold text-slate-600">
-            Name
-            {editMode ? (
-              <Input
-                type="text"
-                value={name}
-                onChange={handleNameChange}
-                placeholder="Change name"
-                className=" bg-slate-200 border-blue-500 border-2 ms-3 font-normal placeholder:text-gray-400 "
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Enter your password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
               />
-            ) : (
-              <p className="font-normal p-3 bg-slate-200 ms-3 rounded-sm">
-                {name}
-              </p>
-            )}
-          </Label>
+              <FormField
+                control={form.control}
+                name="retypePassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Re-type Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Re-type your password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter your name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Save</Button>
+            </form>
+          </Form>
         </div>
-      </div>
+      )}
     </>
   );
 }
