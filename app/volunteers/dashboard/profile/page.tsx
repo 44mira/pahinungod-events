72451ffd67032453602df0b02cf23856d5a2ffcc
Owen Cariño <owenyoungcarino@gmail.com>
2024-05-29 18:47:33 +0800

    fix: code conventions.

--- app/volunteers/dashboard/profile/page.tsx
+++ app/volunteers/dashboard/profile/page.tsx
@@ -32,11 +32,11 @@ export default function Profile() {
     <>
       <div className="w-32 h-32 bg-gray-500 rounded-full mx-auto"></div>
       <div className="pt-10 space-y-7">
-        {ProfileData.map(({ label, value }, idx) => (
-          <div key={idx}>
+        {ProfileData.map(({ label, value }) => (
+          <div key={label}>
             <Label className="text-gray-500">{label}</Label>
             <div className="border-b-[3px] border-gray-300 font-semibold text-lg drop-shadow-md">
-              {value ? value : "No data available."}
+              {value && "No data available"}
             </div>
           </div>
         ))}
