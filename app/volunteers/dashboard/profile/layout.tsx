7566da09d50899e05bcaae4fe9943bbd42394151
Owen Cariño <owenyoungcarino@gmail.com>
2024-05-29 19:08:25 +0800

    style: added back button to profile page and other UX properties.

--- app/volunteers/dashboard/profile/layout.tsx
+++ app/volunteers/dashboard/profile/layout.tsx
@@ -1,7 +1,16 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
 export default function Profile({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
   return (
     <div className="p-5">
-      <div className="text-xl font-bold pb-5">Edit Profile</div>
+      <Button variant={"accent"} onClick={() => router.back()}>
+        Back
+      </Button>
+      <div className="text-xl font-bold py-5">Edit Profile</div>
       {children}
     </div>
   );
