46c8f4747ad74568067c743745033a9ba5084960
44mira <legolaslada@gmail.com>
2024-06-01 17:29:26 +0800

    feat(admin-profile): add logout to profile page

--- app/dashboard/profile/layout.tsx
+++ app/dashboard/profile/layout.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { signout } from "@/actions/auth/actions";
 
 export default function Profile({ children }: { children: React.ReactNode }) {
   return (
@@ -7,22 +8,28 @@ export default function Profile({ children }: { children: React.ReactNode }) {
         <h1 className="text-2xl text-accent font-bold basis-5">Profile</h1>
         <div className="flex w-full items-center gap-5">
           <div className="grow" />
-          <Button
-            className="text-xsm text-accent-foreground"
-            variant={"accent"}
-          >
-            Log out
-          </Button>
+          <form action={signout}>
+            <Button
+              type="submit"
+              className="text-xsm text-accent-foreground"
+              variant={"accent"}
+            >
+              Log out
+            </Button>
+          </form>
         </div>
       </div>
       <div className="flex flex-col items-center gap-2 lg:hidden">
         <div className="flex gap-2 items-center self-end">
-          <Button
-            className="text-xsm text-accent-foreground"
-            variant={"accent"}
-          >
-            Log out
-          </Button>
+          <form action={signout}>
+            <Button
+              type="submit"
+              className="text-xsm text-accent-foreground"
+              variant={"accent"}
+            >
+              Log out
+            </Button>
+          </form>
         </div>
         <h1 className="text-2xl text-accent font-bold mx-auto">Profile</h1>
       </div>
