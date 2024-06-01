3dd549d970003f81e7aeacfd1d6f656689721018
Owen Cariño <owenyoungcarino@gmail.com>
2024-06-01 19:48:33 +0800

    refactor: simplify fetch of data.

--- hooks/use-get-user-identity.ts
+++ hooks/use-get-user-identity.ts
@@ -1,25 +1,18 @@
-import { TypedSupabaseClient } from "@/utils/supabase";
-import useSupabase from "./useSupabase";
-import { User } from "@supabase/supabase-js"; 
 import { useQuery } from "@tanstack/react-query";
-
-
-const fetchUserData = async (supabase: TypedSupabaseClient): Promise<User | null> => {
-  const { data, error } = await supabase.auth.getUser(); // Fetch current user data
-
-  if (error) {
-    throw new Error(error.message);
-  }
-
-  return data.user;
-};
+import useSupabase from "./useSupabase";
 
 export default function useGetUserIdentity() {
   const supabase = useSupabase();
 
-  // Use the useQuery hook to manage the user data fetching
-  return useQuery<User | null, Error>({
-    queryKey: ['user'],
-    queryFn: () => fetchUserData(supabase),
-  });
+  const queryFn = async () => {
+    const { data, error } = await supabase.auth.getUser();
+
+    if (error) {
+      throw "There was an error fetching the user data.";
+    }
+
+    return data.user;
+  };
+
+  return useQuery({ queryKey: ["user"], queryFn });
 }
