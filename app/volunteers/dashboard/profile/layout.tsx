afd8ef4a8552cc9511daa1f13f7cbba6235a2905
Owen Cariño <owenyoungcarino@gmail.com>
2024-06-02 04:41:49 +0800

    feat: add log out button to profile page.

--- app/volunteers/dashboard/profile/layout.tsx
+++ app/volunteers/dashboard/profile/layout.tsx
@@ -2,14 +2,20 @@
 
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import { signoutUser } from "@/actions/auth/actions";
 
 export default function Profile({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   return (
     <div className="p-5">
-      <Button variant={"accent"} onClick={() => router.back()}>
-        Back
-      </Button>
+      <div className="flex justify-between">
+        <Button variant={"accent_gradient"} onClick={() => router.back()}>
+          Back
+        </Button>
+        <form action={signoutUser}>
+          <Button type="submit">Log out</Button>
+        </form>
+      </div>
       <div className="text-xl font-bold py-5">Edit Profile</div>
       {children}
     </div>
