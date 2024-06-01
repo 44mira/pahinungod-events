f1026e10b5d8cae34bbdad79bafb92ad477db920
Owen Cariño <owenyoungcarino@gmail.com>
2024-06-01 22:19:20 +0800

    refactor: upsert user_id into volunteer_id.

--- hooks/use-signin-google-mutation.ts
+++ hooks/use-signin-google-mutation.ts
@@ -15,12 +15,40 @@ export default function useSigninGoogleMutation() {
         redirectTo: location.origin + "/auth/callback",
       },
     });
-        console.log(data.url);
-    
-        if (error) {
-          throw "An error occurred while logging in with Google.";
-        }
+    console.log(data.url);
+
+    if (error) {
+      throw "An error occurred while logging in with Google.";
+    }
   };
+
+  // Fetch the id of the current user from auth table.
+  const queryFn = async () => {
+    const { data, error } = await supabase.auth.getUser();
+
+    if (error) {
+      throw "There was an error fetching the user data.";
+    }
+
+    return data.user.id;
+  };
+
+  // Upserts the id into volunteer_id from volunteer table.
+  const upsertRow = async () => {
+    const user_id = await queryFn();
+
+    const { data, error } = await supabase
+      .from("volunteer")
+      .upsert({ volunteer_id: user_id }); 
+
+    if (error) {
+      throw "There was an error upserting the row.";
+    }
+
+    return data; 
+  };
+
+  upsertRow();
 
   return useMutation({ mutationFn });
 }
