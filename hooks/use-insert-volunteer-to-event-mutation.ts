d8c746180e28593c2652232743f45ae50cb28cf3
Owen Cariño <owenyoungcarino@gmail.com>
2024-06-02 15:53:35 +0800

    chore: prettier format not enabled on previous commits.

--- hooks/use-insert-volunteer-to-event-mutation.ts
+++ hooks/use-insert-volunteer-to-event-mutation.ts
@@ -3,25 +3,23 @@ import useSupabase from "./useSupabase";
 import { UUID } from "crypto";
 
 export default function useInsertVntrToEvent(event_id: UUID, userID: UUID) {
-    const supabase = useSupabase();
+  const supabase = useSupabase();
 
-    const mutationFn = async () => {
-        const { data, error } = await supabase
-        .from('event_volunteer')
-        .insert([
-        { volunteer_id: userID, event_id: event_id },
-        ])
-        .select()
+  const mutationFn = async () => {
+    const { data, error } = await supabase
+      .from("event_volunteer")
+      .insert([{ volunteer_id: userID, event_id: event_id }])
+      .select();
 
-        if (error) {
-            console.log("Error inserting either userID or eventID to event_volunteer table.");
-            throw error;
-        }
-      
-        return data;
-        
+    if (error) {
+      console.log(
+        "Error inserting either userID or eventID to event_volunteer table."
+      );
+      throw error;
     }
-    
-    return useMutation({mutationFn});
-    
+
+    return data;
+  };
+
+  return useMutation({ mutationFn });
 }
