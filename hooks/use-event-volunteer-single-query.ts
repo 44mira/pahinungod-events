b769bee23b5031761e20c26b074b1cb91dc04b44
Owen Cariño <owenyoungcarino@gmail.com>
2024-06-04 23:11:13 +0800

    deps: rename event_volunteer hook.

---
+++ hooks/use-event-volunteer-single-query.ts
@@ -0,0 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import useSupabase from "./useSupabase";
+import { UUID } from "crypto";
+
+export default function useEventVolunteerSingleQuery(
+  event_id: UUID,
+  volunteer_id: UUID
+) {
+  const supabase = useSupabase();
+  const queryKey = ["event_volunteer_data", event_id];
+
+  const queryFn = async () => {
+    const { data, error } = await supabase
+      .from("event_volunteer")
+      .select()
+      .eq("event_id", event_id)
+      .eq("volunteer_id", volunteer_id)
+      .single();
+
+    if (error) {
+      console.log("An error has occurred in fetching event_volunteer data.");
+      throw error;
+    }
+
+    // Returns the object data not in array form.
+    return data;
+  };
+
+  return useQuery({ queryKey, queryFn });
+}
