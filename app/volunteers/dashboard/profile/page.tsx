b06b52b88b9faf065e2aaae7f90cd3bbc63c5a00
44mira <legolaslada@gmail.com>
2024-06-05 20:46:04 +0800

    feat: update volunteer page

--- app/volunteers/dashboard/profile/page.tsx
+++ app/volunteers/dashboard/profile/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Label } from "@/components/ui/label";
 import useSingleUserQuery from "@/hooks/use-single-user-query";
 import Image from "next/image";
 import {
@@ -11,23 +10,32 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import {
+  Select,
+  SelectItem,
+  SelectTrigger,
+  SelectContent,
+  SelectValue,
+} from "@/components/ui/select";
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import pencil_icon from "@/public/pencil_icon.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useVolunteerUpdateMutation from "@/hooks/use-volunteer-update-mutation";
+import { Database } from "@/utils/database.types";
 
 // Define form schema with password match validation
 const formSchema = z.object({
   name: z.string(),
-  nickname: z.string(),
+  nickname: z.string().optional(),
   email: z.string(),
   occupation: z.string().optional(),
   phone_number: z.string().optional(),
   sex: z.string().optional(),
-  age: z.string().optional(),
+  age: z.number(),
 });
 
 type ProfileDataKey =
@@ -39,50 +47,54 @@ type ProfileDataKey =
   | "sex"
   | "age";
 
+type Volunteer = Pick<
+  Database["public"]["Tables"]["volunteer"]["Row"],
+  ProfileDataKey
+>;
+
 export default function Profile() {
-  const { data: volunteer, isLoading, isError } = useSingleUserQuery();
+  const { data: volunteer, status } = useSingleUserQuery();
+  const { mutate } = useVolunteerUpdateMutation();
 
   const [editProfileState, setEditProfileState] = useState(true);
-  const form = useForm<z.infer<typeof formSchema>>({
+  const form = useForm<Volunteer>({
     resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      nickname: "",
+      email: "",
+      occupation: "Student",
+      phone_number: "",
+      sex: "M",
+      age: 0,
+    },
   });
 
-  const ProfileDataTemplate: {
-    label: string;
-    valueKey?: string | number | null;
-    column: ProfileDataKey;
-  }[] = [
-    { label: "Full name", valueKey: volunteer?.name, column: "name" },
-    { label: "Nickname", valueKey: volunteer?.nickname, column: "nickname" },
-    { label: "E-mail", valueKey: volunteer?.email, column: "email" },
-    {
-      label: "Occupation",
-      valueKey: volunteer?.occupation,
-      column: "occupation",
-    },
-    {
-      label: "Phone No.",
-      valueKey: volunteer?.phone_number,
-      column: "phone_number",
-    },
-    { label: "Sex", valueKey: volunteer?.sex, column: "sex" },
-    { label: "Age", valueKey: volunteer?.age, column: "age" },
-  ];
+  useEffect(() => {
+    form.setValue("name", volunteer?.name ?? "No name provided");
+    form.setValue("nickname", volunteer?.nickname ?? "No nickname provided");
+    form.setValue("email", volunteer?.email ?? "No email provided");
+    form.setValue("occupation", volunteer?.occupation ?? "Student");
+    form.setValue("phone_number", volunteer?.phone_number ?? "");
+    form.setValue("sex", volunteer?.sex ?? "M");
+    form.setValue("age", volunteer?.age ?? 0);
+  }, [form, volunteer]);
 
-  if (isLoading) {
+  const occupation = ["Student", "Alumni", "Faculty", "Admin Staff", "Retiree"];
+
+  if (status === "pending") {
     return <div>Loading...</div>;
   }
 
-  if (isError) {
+  if (status === "error") {
     return <div>Error loading user data.</div>;
   }
 
   const userMetaData = volunteer?.userData.user.user_metadata;
-
   const profilePicDimesnsion = 250;
 
   return (
-    <>
+    <div className="flex flex-col gap-5">
       <div className="w-32 h-32 bg-gray-500 rounded-full mx-auto">
         <Image
           src={userMetaData?.picture}
@@ -103,42 +115,164 @@ export default function Profile() {
           Edit Profile
         </Button>
       </span>
-      <div className="pt-10 space-y-7">
+      <div className="space-y-7">
         <Form {...form}>
           <form
-            /* onSubmit={form.handleSubmit((data) => {
-                mutate(data);
-                setEditProfileState(false);
-              })} */
+            onSubmit={form.handleSubmit((data) => {
+              mutate(data);
+              setEditProfileState(true);
+            })}
             className="space-y-8"
           >
-            {ProfileDataTemplate.map(({ label, valueKey, column }) => (
-              <FormField
-                key={column}
-                control={form.control}
-                name={column}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>{label}</FormLabel>
-                    <FormControl>
-                      <Input
-                        disabled={editProfileState}
-                        placeholder={valueKey?.toString()}
-                        {...field}
-                        className="bg-slate-200 disabled:bg-slate-200 placeholder:text-black placeholder:bg-slate-200 disabled:cursor-default"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            ))}
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={editProfileState}
+                      {...field}
+                      className="bg-background disabled:bg-slate-200 placeholder:text-black disabled:cursor-default"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="nickname"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nickname</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={editProfileState}
+                      {...field}
+                      value={field.value!}
+                      className="bg-background disabled:bg-slate-200 placeholder:text-black disabled:cursor-default"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>E-mail</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled
+                      {...field}
+                      className="bg-background disabled:bg-slate-200 placeholder:text-black disabled:cursor-default"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="occupation"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Occupation</FormLabel>
+                  <FormControl>
+                    <Select
+                      onValueChange={field.onChange}
+                      disabled={editProfileState}
+                    >
+                      <FormControl>
+                        <SelectTrigger className="bg-background disabled:bg-slate-200">
+                          <SelectValue placeholder={field.value} />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {occupation.map((value, index) => (
+                          <SelectItem key={index} value={value}>
+                            {value}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="phone_number"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Phone No.</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={editProfileState}
+                      {...field}
+                      className="bg-background disabled:bg-slate-200 placeholder:text-black disabled:cursor-default"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="sex"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Gender</FormLabel>
+                  <FormControl>
+                    <Select
+                      onValueChange={field.onChange}
+                      disabled={editProfileState}
+                    >
+                      <FormControl>
+                        <SelectTrigger className="bg-background disabled:bg-slate-200">
+                          <SelectValue placeholder={field.value} />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="M"> Male </SelectItem>
+                        <SelectItem value="F">Female</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="age"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Age</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={editProfileState}
+                      {...field}
+                      value={field.value!}
+                      onChange={(e) => field.onChange(+e.target.value)}
+                      className="bg-background disabled:bg-slate-200 placeholder:text-black disabled:cursor-default"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" variant={"outline"} className="w-fit">
+              Save
+            </Button>
           </form>
         </Form>
       </div>
-      <Button type="submit" variant={"outline"}>
-        Save
-      </Button>
-    </>
+    </div>
   );
 }
