7c6972037a9f608727320080125c69edd4315dbb
Owen Cariño <owenyoungcarino@gmail.com>
2024-06-05 18:14:56 +0800

    feat: active, upcoming, past template for dashboard

--- app/volunteers/dashboard/(dashboard-page)/page.tsx
+++ app/volunteers/dashboard/(dashboard-page)/page.tsx
@@ -13,10 +13,13 @@ import CalendarWhite from "@/public/calendar_white";
 import { Button } from "@/components/ui/button";
 import useDashboardQuery from "@/hooks/use-dashboard-query";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Dashboard() {
   // Fetch data from database
   const { data: events, status } = useDashboardQuery();
+
+  const [activeEvent, setActiveStatus] = useState(false);
 
   // If fetching is successful
   if (status === "pending")
@@ -30,38 +33,82 @@ export default function Dashboard() {
     volunteerCap: event.volunteer_cap, // 'volunteer_cap' column in a single row iterated.
   }));
 
+  const currentDate = formatDate(new Date().toISOString());
+
   return (
-    <div className="space-y-9">
-      {eventInfo?.map((event, index) => (
-        <Card key={index}>
-          <CardHeader>
-            <CardTitle className="text-2lg">{event.title}</CardTitle>
-          </CardHeader>
-          <CardContent className="flex items-center border-y-2 border-accent/50 pb-0 mb-3">
-            <CardDescription className="flex items-center gap-3 font-semibold text-md border-r-[3px] border-accent/50 pr-4">
-              <UsersWhite className="text-accent-strong" />
-              <span>{event.volunteerCap}</span>
-            </CardDescription>
-            <CardDescription className=" flex items-center gap-3 ps-1 font-semibold text-md">
-              <div className=" text-accent-strong">
-                <CalendarWhite />
-              </div>
-              <span>{event.date}</span>
-            </CardDescription>
-          </CardContent>
-          <CardFooter>
-            <Link
-              href={{ pathname: `/volunteers/dashboard/events/${event.id}` }}
-              className="w-full"
-            >
-              <Button className=" w-full bg-gradient-to-l from-accent-strong to-accent-light from-5% to-95%">
-                View
-              </Button>
-            </Link>
-          </CardFooter>
-        </Card>
-      ))}
-    </div>
+    <>
+      <div className="space-y-9">
+        <p className="text-lg font-semibold">Active Events</p>
+        {eventInfo?.map((event, index) =>
+          currentDate == event.date ? (
+            <Card key={index}>
+              <CardHeader>
+                <CardTitle className="text-2lg">{event.title}</CardTitle>
+              </CardHeader>
+              <CardContent className="flex items-center border-y-2 border-accent/50 pb-0 mb-3">
+                <CardDescription className="flex items-center gap-3 font-semibold text-md border-r-[3px] border-accent/50 pr-4">
+                  <UsersWhite className="text-accent-strong" />
+                  <span>{event.volunteerCap}</span>
+                </CardDescription>
+                <CardDescription className=" flex items-center gap-3 ps-1 font-semibold text-md">
+                  <div className=" text-accent-strong">
+                    <CalendarWhite />
+                  </div>
+                  <span>{event.date}</span>
+                </CardDescription>
+              </CardContent>
+              <CardFooter>
+                <Link
+                  href={{
+                    pathname: `/volunteers/dashboard/events/${event.id}`,
+                  }}
+                  className="w-full"
+                >
+                  <Button className=" w-full bg-gradient-to-l from-accent-strong to-accent-light from-5% to-95%">
+                    View
+                  </Button>
+                </Link>
+              </CardFooter>
+            </Card>
+          ) : (
+            ""
+          )
+        )}
+        {activeEvent}
+      </div>
+      <div className="space-y-9 mt-20">
+        <p className="text-lg font-semibold">Upcoming Events</p>
+        {eventInfo?.map((event, index) => (
+          <Card key={index}>
+            <CardHeader>
+              <CardTitle className="text-2lg">{event.title}</CardTitle>
+            </CardHeader>
+            <CardContent className="flex items-center border-y-2 border-accent/50 pb-0 mb-3">
+              <CardDescription className="flex items-center gap-3 font-semibold text-md border-r-[3px] border-accent/50 pr-4">
+                <UsersWhite className="text-accent-strong" />
+                <span>{event.volunteerCap}</span>
+              </CardDescription>
+              <CardDescription className=" flex items-center gap-3 ps-1 font-semibold text-md">
+                <div className=" text-accent-strong">
+                  <CalendarWhite />
+                </div>
+                <span>{event.date}</span>
+              </CardDescription>
+            </CardContent>
+            <CardFooter>
+              <Link
+                href={{ pathname: `/volunteers/dashboard/events/${event.id}` }}
+                className="w-full"
+              >
+                <Button className=" w-full bg-gradient-to-l from-accent-strong to-accent-light from-5% to-95%">
+                  View
+                </Button>
+              </Link>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </>
   );
 }
 
