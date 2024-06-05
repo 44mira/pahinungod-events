7ebc9f4c33be449cc71d28019eabf446b87414e7
44mira <legolaslada@gmail.com>
2024-06-05 13:14:58 +0800

    refactor: remove extraneous maps

--- app/volunteers/dashboard/events/page.tsx
+++ app/volunteers/dashboard/events/page.tsx
@@ -23,56 +23,42 @@ export default function RegisteredEvents() {
   if (status === "pending")
     return <p className="text-accent text-2lg font-bold">Loading...</p>; // Will replace with skeleton
 
-  // Iterate all the row in the events that the user registered.
-  const eventInfo = registeredEvents?.events?.map((event) => ({
-    id: event.event_id, // 'event_id' column in a single row iterated.
-    title: event.name, // 'name' column in a single row iterated.
-    date: formatDate(event.event_start), //  'event_start' column in a single row iterated.
-    volunteerCap: event.volunteer_cap, // 'volunteer_cap' column in a single row iterated.
-  }));
-
-  // Iterate all the row in the event_volunteer table that matches the user id.
-  const eventStatus = registeredEvents?.eventVolunteer.map((event) => ({
-    eventId: event.event_id,
-    status: event.status,
-  }));
-
   return (
     <div className="space-y-9">
-      {eventInfo?.map((event, index) => (
-        <Card key={index}>
-          <CardHeader>
-            <CardTitle className="text-2lg">{event.title}</CardTitle>
-          </CardHeader>
-          <CardContent className="flex items-center border-y-2 border-accent/50 pb-0 mb-3">
-            <CardDescription className="flex items-center gap-3 font-semibold text-md border-r-[3px] border-accent/50 pr-4">
-              <UsersWhite className="text-accent-strong" />
-              <span>{event.volunteerCap}</span>
-            </CardDescription>
-            <CardDescription className=" flex items-center gap-3 ps-1 font-semibold text-md">
-              <div className=" text-accent-strong">
-                <CalendarWhite />
-              </div>
-            </CardDescription>
-          </CardContent>
-          {eventStatus?.map((status) => (
-            <CardDescription key={status.eventId}>
-              {status.status}
-            </CardDescription>
-          ))}
-          <CardFooter>
-            <Link
-              href={{ pathname: `/volunteers/dashboard/events/${event.id}` }}
-              className="w-full"
-            >
-              <Button className=" w-full bg-gradient-to-l from-accent-strong to-accent-light from-5% to-95%">
-                View
-              </Button>
-            </Link>
-          </CardFooter>
-        </Card>
-      ))}
+      {registeredEvents?.events!.map(
+        ({ event_id, name, event_start, volunteer_cap }) => (
+          <Card key={event_id}>
+            <CardHeader>
+              <CardTitle className="text-2lg">{name}</CardTitle>
+            </CardHeader>
+            <CardContent className="flex items-center border-y-2 border-accent/50 pb-0 mb-3">
+              <CardDescription className="flex items-center gap-3 font-semibold text-md border-r-[3px] border-accent/50 pr-4">
+                <UsersWhite className="text-accent-strong" />
+                <span>{volunteer_cap}</span>
+              </CardDescription>
+              <CardDescription className=" flex items-center gap-3 ps-1 font-semibold text-md">
+                <div className=" text-accent-strong">
+                  <CalendarWhite />
+                </div>
                 <span>{moment(event_start).format("MMM D YYY")}</span>
+              </CardDescription>
+            </CardContent>
+            {registeredEvents?.eventVolunteer.map(({ event_id, status }) => (
+              <CardDescription key={event_id}>{status}</CardDescription>
+            ))}
+            <CardFooter>
+              <Link
+                href={{ pathname: `/volunteers/dashboard/events/${event_id}` }}
+                className="w-full"
+              >
+                <Button className=" w-full bg-gradient-to-l from-accent-strong to-accent-light from-5% to-95%">
+                  View
+                </Button>
+              </Link>
+            </CardFooter>
+          </Card>
+        ),
+      )}
     </div>
   );
 }
